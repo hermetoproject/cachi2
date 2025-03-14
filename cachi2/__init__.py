a2dfc3b9c6687de9f62d850d44eb097e5de424f0
Erik Skultety <eskultet@redhat.com>
2025-03-14 14:44:40 +0100

    Rebranding: Define a global APP_NAME variable
    This global variable is to be used in future rebranding patches in
    all user-facing error strings and all name-bound strings/logic that we
    have to help us make the rebranding transparent to end users and not
    break backwards compatibility because of renaming.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>

---
+++ cachi2/__init__.py
@@ -0,0 +1,22 @@
+import importlib.util as imputil
+import sys
+from functools import cache
+from pathlib import Path
+
+
+@cache
+def _get_app_name() -> str:
+    """
+    Get main application name.
+
+    Handles invocations via all supported CLI script names. Falls back to using the name of the
+    installed package (e.g. during pytest invocations) or a hardcoded value if spec is not
+    available (to satisfy mypy).
+    """
+    if (name := Path(sys.argv[0]).name) not in ("cachi2",):
+        spec = imputil.find_spec(__name__)
+        name = spec.name if spec else "cachi2"
+    return name
+
+
+APP_NAME = _get_app_name()
